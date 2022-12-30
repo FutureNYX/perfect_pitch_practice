4e4c291ce594a546bb59bacfc88dd8aa7cbea39c
FutureNYX <121621468+FutureNYX@users.noreply.github.com>
2022-12-30 12:54:53 +0000

    version 1

---
+++ src/GuessBox.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react'
+import noteDict from './assets';
+
+export const GuessBox = () => {
+
+  const [noteBtnDisplay, setNoteBtnDisplay] = useState('play');
+
+  const getRandomNoteKey = (selection) => {
+    const key_array = Object.keys(noteDict)
+    const selection_filter = (selection) => {
+      if(selection === 'white'){return key_array.filter(key => !key.includes('b'))}
+      else if(selection === 'black'){return key_array.filter(key => key.includes('b'))}
+      else if(selection === 'all'){return key_array}
+    }
+    return selection_filter(selection)[Math.floor(Math.random()*selection_filter(selection).length)]
+  }
+
+  const noteRange = ['all', 'white', 'black'];
+  const progressionBtn = ['reveal', 'another'];
+  const [rangeName, setRange] = useState(noteRange[0]);
+  const [progressionBtnName, setProgressionBtn] = useState(progressionBtn[0]);
+  const [noteKey, setNoteKey] = useState(()=>getRandomNoteKey('all'));
+  
+  const playNote = () => {
+    new Audio(noteDict[noteKey]).play()
+  }
+
+  const array_cycler = (array, arrayItem) => {
+   return array.indexOf(arrayItem) === array.length-1 ? array[0] : array[array.indexOf(arrayItem)+1]
+  }
+
+  return (
+    <div>
+      <button onClick={playNote}>{noteBtnDisplay}</button>
+      <button onClick={()=>{if(progressionBtnName === 'reveal'){
+        setNoteBtnDisplay(noteKey);
+        setProgressionBtn('another')
+        }
+        else{
+          setNoteKey(getRandomNoteKey(rangeName));
+          setNoteBtnDisplay('play');
+          setProgressionBtn('reveal')
+        }}}>{progressionBtnName}</button>
+      <button onClick={()=>{setRange(array_cycler(noteRange,rangeName));
+      setNoteKey(getRandomNoteKey(rangeName));
+          setNoteBtnDisplay('play');
+          setProgressionBtn('reveal')}}>{rangeName} keys</button>
+    </div>
+  )
+}
